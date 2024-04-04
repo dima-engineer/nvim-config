6962cc8efc9a9d026a12b55588beb91c1e4e7758
Dmitry Marchuk <marchuk.d13@gmail.com>
2024-04-04 23:13:21 +0200

    Add plugins file and replace there lazy config

---
+++ lua/user/plugins.lua
@@ -0,0 +1,40 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup lazy_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | Lazy sync
+  augroup end
+]]
+
+local status_ok, lazy = pcall(require, "lazy")
+if not status_ok then
+  return
+end
+
+lazy.setup({
+	{"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
+  "nvim-lua/popup.nvim",  -- An implementation of the Popup API from vim in Neovim
+  "nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+})
